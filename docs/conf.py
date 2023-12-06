bad9916205c0c2aa1190a3c074a0a5c42ce9c9fc
Travis Hathaway <travis.j.hathaway@gmail.com>
2023-12-06 10:24:01 +0100

    adding components for the footer link

--- docs/conf.py
+++ docs/conf.py
@@ -61,6 +61,10 @@ html_theme_options = {
     ],
 }
 
+html_context = {
+    "goatcounter_dashboard_url": ""  # Link to your GoatCounter dashboard
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
