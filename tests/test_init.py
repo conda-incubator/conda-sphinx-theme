0849376f0bc2a268bfd5bcd1023f3872db4c64b2
Jannis Leidel <jannis@leidel.info>
2025-10-24 15:21:49 +0200

    Apply suggestion from @jezdez

--- tests/test_init.py
+++ tests/test_init.py
@@ -19,20 +19,3 @@ def test_module_imports():
     assert len(conda_sphinx_theme.__version__) > 0
 
 
-def test_get_html_theme_path():
-    """Test the get_html_theme_path function."""
-    import conda_sphinx_theme
-    
-    # The function should return the path to the theme directory
-    theme_path = conda_sphinx_theme.get_html_theme_path()
-    
-    # Should return a list containing one path
-    assert isinstance(theme_path, list)
-    assert len(theme_path) == 1
-    
-    # The path should exist
-    assert os.path.exists(theme_path[0])
-    
-    # The path should contain the theme files
-    theme_dir = theme_path[0]
-    assert os.path.exists(os.path.join(theme_dir, 'conda_sphinx_theme'))
